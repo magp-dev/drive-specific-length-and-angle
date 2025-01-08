fd77b99fd4b7de74fdbd20af61ec221ed4274a24
magp-dev <magnus.paulsen@gmail.com>
2025-01-08 18:07:21 +0100

    Justrering endret til 18%

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    basic.pause(1000)
     bitbot.goms(BBDirection.Forward, 60, Avstand1 * Drivingspeedper1m)
     bitbot.rotatems(BBRobotDirection.Left, 60, Turningspeed360 * (Vinkel1 / 360))
     bitbot.goms(BBDirection.Forward, 60, Avstand2 * Drivingspeedper1m)
@@ -9,9 +10,9 @@ let Avstand1 = 0
 let Turningspeed360 = 0
 let Drivingspeedper1m = 0
 Drivingspeedper1m = 3600
-Turningspeed360 = 1200
+Turningspeed360 = 1600
 bitbot.select_model(BBModel.XL)
-bitbot.BBBias(BBRobotDirection.Left, 10)
+bitbot.BBBias(BBRobotDirection.Left, 18)
 Avstand1 = 2
 Avstand2 = 1
 Vinkel1 = 90
