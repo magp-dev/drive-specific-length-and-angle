f523b96ab695e85d148ba5f14ebfc6a474d6d255
magp-dev <magnus.paulsen@gmail.com>
2025-01-30 20:30:44 +0100

    Føniks: tilpasset ny bitbot XL

--- main.ts
+++ main.ts
@@ -1,18 +1,47 @@
+function kjørBane () {
+    bitbot.goms(BBDirection.Forward, 40, Avstand1 * Drivingspeedper1m)
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.rotatems(BBRobotDirection.Right, 40, Turningspeed360 * (Vinkel1 / 360))
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.goms(BBDirection.Forward, 40, Avstand2 * Drivingspeedper1m)
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.rotatems(BBRobotDirection.Right, 40, Turningspeed360 * (Vinkel2 / 360))
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.goms(BBDirection.Forward, 40, Avstand3 * Drivingspeedper1m)
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.rotatems(BBRobotDirection.Left, 40, Turningspeed360 * (Vinkel2 / 360))
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.goms(BBDirection.Forward, 40, Avstand3 * Drivingspeedper1m)
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.rotatems(BBRobotDirection.Right, 40, Turningspeed360 * (Vinkel1 / 360))
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.goms(BBDirection.Forward, 40, Avstand3 * Drivingspeedper1m)
+}
 input.onButtonPressed(Button.A, function () {
-    basic.pause(1000)
-    bitbot.goms(BBDirection.Forward, 60, Avstand1 * Drivingspeedper1m)
-    bitbot.rotatems(BBRobotDirection.Left, 60, Turningspeed360 * (Vinkel1 / 360))
-    bitbot.goms(BBDirection.Forward, 60, Avstand2 * Drivingspeedper1m)
+    kjørBane()
+    bitbot.goms(BBDirection.Forward, 40, Avstand1 * Drivingspeedper1m)
+    kjørBane()
 })
+input.onButtonPressed(Button.B, function () {
+    basic.pause(1000)
+    bitbot.goms(BBDirection.Forward, 40, Avstand2 * Drivingspeedper1m)
+    bitbot.stop(BBStopMode.Coast)
+    bitbot.rotatems(BBRobotDirection.Left, 40, Turningspeed360 * (Vinkel1 / 360))
+})
+let Vinkel2 = 0
 let Vinkel1 = 0
+let Avstand3 = 0
 let Avstand2 = 0
 let Avstand1 = 0
 let Turningspeed360 = 0
 let Drivingspeedper1m = 0
 Drivingspeedper1m = 3600
-Turningspeed360 = 1600
+Turningspeed360 = 1350
 bitbot.select_model(BBModel.XL)
-bitbot.BBBias(BBRobotDirection.Left, 18)
-Avstand1 = 2
-Avstand2 = 1
+bitbot.BBBias(BBRobotDirection.Right, 8)
+Avstand1 = 0.6
+Avstand2 = 0.5
+Avstand3 = 0.25
+let Avstand4 = 0.2
 Vinkel1 = 90
+Vinkel2 = 45
